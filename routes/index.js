dd0115902611e3565ba4037b9d34f9967a24f9aa
borajimin <borajimin@gmail.com>
2018-04-17 17:04:44 -0700

    work work work

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var router = express.Router()
 
 router.get("/", function(req, res, next) {
-	res.render("index", { title: "Moose's very special app" })
+	res.render("index", { title: "Debi's very special app" })
 })
 
 module.exports = router
